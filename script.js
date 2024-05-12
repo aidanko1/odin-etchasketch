d4fa027735ee7ad032fe1ac6800497ed61f07e24
aidanko1 <aidanko111@gmail.com>
2024-05-12 21:26:05 +0100

    grid function successfully implemented to index.html

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-/*
-Goal: create a 16x16 grid using functions on .js
-1. defining the size of the grid
-2. declare a function
-3. using loop to creates 16 rows
-3. create 16 grids cells per row
-4. append the grid cells in to each row
-5. append the whole grid into <div class="container">
-*/
-
-
 // defining size of the grid
-const CONTAINER = document.querySelector('container');
+const CONTAINER = document.querySelector('#container');
 const GRID_SIZE = 16;
 
 // function: creating the grid
@@ -19,15 +8,14 @@ function createGrid () {
         const ROW = document.createElement('div');
         ROW.className = "row";
     
-        for (let j = 0; j < GRID_SIZE, j++) {
+        for (let j = 0; j < GRID_SIZE; j++) {
             const CELL = document.createElement('div');
             CELL.className = "cell";
             ROW.appendChild(CELL);
         }
 
-        CONTAINER.appendChild("ROW");
+        CONTAINER.appendChild(ROW);
     }
 }
 
 createGrid();
-
