6f488e0c19b2bcfcb1fc85d2c08913d85f16f359
aidanko1 <aidanko111@gmail.com>
2024-05-14 13:00:15 +0100

    implemented button function: change grid size

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 // defining size of the grid
 const CONTAINER = document.querySelector('#container');
-const GRID_SIZE = 16;
+let gridSize = 16;
 
 // function: creating the grid
 function createGrid () {
-    for (let i = 0; i < GRID_SIZE; i++) {
+    CONTAINER.innerHTML = '';
+
+    const cellSize = 960 / gridSize;
+    
+    for (let i = 0; i < gridSize; i++) {
         const ROW = document.createElement('div');
         ROW.className = "row";
     
-        for (let j = 0; j < GRID_SIZE; j++) {
+        for (let j = 0; j < gridSize; j++) {
             const CELL = document.createElement('div');
             CELL.className = "cell";
+            CELL.style.width = cellSize + 'px';
+            CELL.style.height = cellSize + 'px';
             CELL.addEventListener('mouseover', () => paintCell(CELL));
             ROW.appendChild(CELL);
         }
@@ -25,3 +31,25 @@ createGrid();
 function paintCell(cell) {
     cell.style.backgroundColor = 'black';
 }
+
+// create button element on the top of the page
+const BUTTON = document.querySelector('#changeSize');
+
+function promptGridSize () {
+    console.log("prompt function called");
+    let newGridSize = prompt('Enter the number of squares per side (max 100)');
+    newGridSize = parseInt(newGridSize);
+
+    if (isNaN(newGridSize)) {
+        alert('Invalid input. Please enter a valid number');
+        return;
+    } else if (newGridSize < 1 || newGridSize > 100) {
+        alert('Invalid input. Please a number between 1 and 100.');
+        return;   
+    } else {
+        gridSize = newGridSize;
+        createGrid();
+    }
+}
+
+BUTTON.addEventListener('click', promptGridSize);
